ceb2299849a543dc54ac91459a9629a37eb7aef7
lucky634634 <khoa21102003@gmail.com>
2024-12-20 11:13:24 +0700

    Update neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -11,5 +11,21 @@ return {
         window = {
             position = "right",
         },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {
+                    '.git',
+                    -- '.DS_Store',
+                    -- 'thumbs.db',
+                },
+                never_show = {
+                    '.git'
+                },
+            },
+        },
     },
 }
