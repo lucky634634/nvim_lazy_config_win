d85e62cecd7ce07d2cbee910d8767c0e2a4987a0
lucky634634 <khoa21102003@gmail.com>
2024-12-22 20:51:55 +0700

    Update

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,29 @@
 return {
-	"terrortylor/nvim-comment",
-	config = function()
-		local comment = require("nvim_comment")
-		comment.setup()
-	end,
+    "numToStr/Comment.nvim",
+    config = function()
+        require("Comment").setup({
+            padding = true,
+            sticky = true,
+            ignore = nil,
+            toggle = {
+                line = "gcc",
+                block = "gbc",
+            },
+            opleader = {
+                line = "gc",
+                block = "gb",
+            },
+            extra = {
+                above = "gcO",
+                below = "gco",
+                eol = "gcA",
+            },
+            mappings = {
+                basic = true,
+                extra = true,
+            },
+            pre_hook = nil,
+            post_hook = nil,
+        })
+    end,
 }
