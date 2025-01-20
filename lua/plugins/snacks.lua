675f31d4d79a27528e55f240c89b81469010bb7a
lucky634634 <khoa21102003@gmail.com>
2025-01-20 22:49:54 +0700

    Update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,9 +3,6 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         bigfile = { enabled = true },
         dashboard = { enabled = true },
         indent = { enabled = true },
@@ -15,5 +12,14 @@ return {
         scroll = { enabled = true },
         statuscolumn = { enabled = true },
         words = { enabled = true },
+    },
+    keys = {
+        {
+            "<c-/>",
+            function()
+                Snacks.terminal()
+            end,
+            desc = "Toggle Terminal",
+        },
     },
 }
