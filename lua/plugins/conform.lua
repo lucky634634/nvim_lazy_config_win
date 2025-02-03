40f1bbe06a6288c9914b393673c550e1630b25d8
lucky634634 <khoa21102003@gmail.com>
2025-02-03 08:08:50 +0700

    Update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
         conform.setup({
             formatters_by_ft = {
                 lua = { "stylua" },
+                json = { "prettier" },
             },
             format_on_save = {
                 lsp_fallback = true,
