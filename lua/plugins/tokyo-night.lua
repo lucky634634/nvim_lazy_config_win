b99fc23d4e70bd31be3bcb05b6518906195867fb
lucky634634 <khoa21102003@gmail.com>
2025-02-09 16:33:30 +0700

    Update

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -3,4 +3,14 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
+    config = function()
+        require("tokyonight").setup({
+            style = "night",
+            transparent = true,
+            on_colors = function(colors)
+                colors.hint = colors.orange
+                colors.error = "#990000"
+            end,
+        })
+    end,
 }
