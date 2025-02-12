ce956175c23424bfa174268c46faa948fb7271cb
lucky634634 <khoa21102003@gmail.com>
2025-02-12 16:04:29 +0700

    Update

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,7 +1,7 @@
 return {
     "Exafunction/codeium.vim",
     event = "BufEnter",
-    version = "1.20.0",
+    version = "1.20.8",
     config = function()
         vim.g.codeium_disable_bindings = false
         vim.g.codeium_enabled = true
