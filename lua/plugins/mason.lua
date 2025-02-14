951c4d66e5c46b6eb845eb91fbc43a8a853df011
lucky634634 <khoa21102003@gmail.com>
2025-02-14 20:17:11 +0700

    Update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,21 +6,74 @@ return {
         "neovim/nvim-lspconfig",
     },
     config = function()
-        require("mason").setup()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool = require("mason-tool-installer")
+        local lsp = require("lspconfig")
+        mason.setup()
 
-        require("mason-lspconfig").setup({
+        mason_lspconfig.setup({
             automatic_installation = true,
             ensure_installed = {
                 "lua_ls",
             },
         })
 
-        require("mason-tool-installer").setup({
+        mason_tool.setup({
             ensure_installed = {
                 "prettier",
                 "stylua", -- lua formatter
                 "lua_ls",
             },
         })
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = function(str)
+                    return { buffer = ev.buf, desc = str }
+                end
+
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts("Declaration"))
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts("Definition"))
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts("Implementation"))
+                vim.keymap.set('n', '<leader>F', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts("Format Buffer"))
+            end,
+        })
+
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+        mason_lspconfig.setup_handlers({
+            function(server)
+                lsp[server].setup({
+                    capabilities = capabilities
+                })
+            end
+        })
+
+        lsp.lua_ls.setup {
+            settings = {
+                Lua = {
+                    runtime = {
+                        path = vim.split(package.path, ';'),
+                    },
+                    diagnostics = { globals = { 'vim', 'hs' }, },
+                    workspace = {
+                        library = {
+                            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                        },
+                    },
+                },
+            },
+        }
     end,
 }
