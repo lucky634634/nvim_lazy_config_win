7ee4078af40e3b7849740b604585fcd13e3b2d93
lucky634634 <khoa21102003@gmail.com>
2025-02-14 11:33:07 +0700

    Update

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,17 +1,6 @@
 local keymap = vim.keymap
 
 keymap.set({ "n", "v" }, "<leader>d", '"_', { silent = true })
-keymap.set("i", "<C-v>", '<Esc>"+pa', { silent = true })
-
-keymap.set("n", "<leader>e", "<CMD>Neotree toggle<CR>", { desc = "Neotree toggle" })
-keymap.set("n", "<leader>r", "<CMD>Neotree focus<CR>", { desc = "Neotree focus" })
-
--- Telescope
-keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", { desc = "Find help" })
-keymap.set("n", "<leader>fc", "<CMD>Telescope colorscheme<CR>", { desc = "Colorscheme picker" })
 
 -- Split
 keymap.set("n", "<leader>o", "<CMD>vsplit<CR>", { silent = true })
@@ -33,26 +22,3 @@ keymap.set("n", "<C-Down>", "<C-w>-", { silent = true })
 keymap.set("n", "<leader>t", "<CMD>tabnew<CR>", { silent = true })
 keymap.set("n", "H", "<CMD>tabprevious<CR>", { silent = true })
 keymap.set("n", "L", "<CMD>tabnext<CR>", { silent = true })
-
--- Format
-keymap.set({ "n", "v" }, "<leader>F", function()
-    local conform = require("conform")
-    conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-    })
-end)
-
--- Terminal
-keymap.set("t", "<esc>", "<C-\\><C-n>", { silent = true })
-
--- Code action
-keymap.set("n", "<leader>ca", function()
-    require("tiny-code-action").code_action()
-end, { noremap = true, silent = true })
-
-keymap.set("n", "<C-\\>", function()
-    local snacks = require("snacks")
-    snacks.Terminal.toggle()
-end, { noremap = true, silent = true })
