801b5b93d33e08e027ccb6e1c9792b457cdc2d6f
lucky634634 <khoa21102003@gmail.com>
2025-02-14 17:02:26 +0700

    Update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -22,23 +22,5 @@ return {
                 "lua_ls",
             },
         })
-
-        local nvim_lsp = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-
-        local protocol = require("vim.lsp.protocol")
-
-        local on_attach = function(client, bufnr)
-            -- format on save
-            if client.server_capabilities.documentFormattingProvider then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                })
-            end
-        end
     end,
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -44,3 +44,9 @@ o.shellxquote = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
 o.shellquote = ""
 o.shellpipe = "| Out-File -Encoding UTF8 %s"
 o.shellredir = "| Out-File -Encoding UTF8 %s"
+
+-- Set up pretty unicode diagnostic signs
+vim.fn.sign_define("DiagnosticSignError", {text = "", hl = "DiagnosticSignError", texthl = "DiagnosticSignError", culhl = "DiagnosticSignErrorLine"})
+vim.fn.sign_define("DiagnosticSignWarn", {text = "", hl = "DiagnosticSignWarn", texthl = "DiagnosticSignWarn", culhl = "DiagnosticSignWarnLine"})
+vim.fn.sign_define("DiagnosticSignInfo", {text = "", hl = "DiagnosticSignInfo", texthl = "DiagnosticSignInfo", culhl = "DiagnosticSignInfoLine"})
+vim.fn.sign_define("DiagnosticSignHint", {text = "", hl = "DiagnosticSignHint", texthl = "DiagnosticSignHint", culhl = "DiagnosticSignHintLine"})
