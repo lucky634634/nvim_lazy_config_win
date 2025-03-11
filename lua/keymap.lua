c8f1a461d42ae7a4e353600251667a6de7f7bb48
lucky634634 <khoa21102003@gmail.com>
2025-03-11 21:31:43 +0700

    Update

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -34,3 +34,5 @@ keymap.set("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", { desc = "Find hel
 keymap.set("n", "<leader>fc", "<CMD>Telescope colorscheme<CR>", { desc = "Colorscheme picker" })
 
 keymap.set('i', '<C-Space>', ':lua MiniCompletion.complete_fallback()<CR>', { silent = true })
+
+keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { silent = true })
