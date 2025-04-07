d809ed42126bce89ed061e55eea71fb382c688bc
lucky634634 <khoa21102003@gmail.com>
2025-04-07 12:34:03 +0700

    Update

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,8 +3,11 @@ return {
     dependencies = { "echasnovski/mini.icons" },
     opts = {},
     keys = {
-        { "<leader>ff", "<cmd>FzfLua files<cr>",    desc = "Files (fzf-lua)" },
-        { "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "Help Tags (fzf-lua)" },
-        { "<leader>fg", "<cmd>FzfLua grep<cr>",     desc = "Grep (fzf-lua)" },
+        { "<leader>ff", "<cmd>FzfLua files<cr>",        desc = "Files (fzf-lua)" },
+        { "<leader>fh", "<cmd>FzfLua helptags<cr>",     desc = "Help Tags (fzf-lua)" },
+        { "<leader>fg", "<cmd>FzfLua grep_visual<cr>",  desc = "Grep (fzf-lua)" },
+        { "<leader>fb", "<cmd>FzfLua buffers<cr>",      desc = "buffers (fzf-lua)" },
+        { "<leader>fc", "<cmd>FzfLua colorschemes<cr>", desc = "ColorSchemes (fzf-lua)" },
+        { "<leader>fk", "<cmd>FzfLua keymaps<cr>",      desc = "keymaps (fzf-lua)" },
     }
 }
