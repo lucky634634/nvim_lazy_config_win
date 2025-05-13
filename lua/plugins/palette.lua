4eb81d2490ade04fad902a313000b3c50fc3dba5
lucky634634 <khoa21102003@gmail.com>
2025-05-13 10:04:53 +0700

    Update

--- lua/plugins/palette.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "bhugovilela/palette.nvim",
-    config = function()
-        vim.g.palette_config = {
-            export_path = vim.fn.stdpath("config") .. '/colors',
-        }
-    end
-}
