8914928a08d66afee1780428bbda65beec975ff2
lucky634634 <khoa21102003@gmail.com>
2025-05-19 08:29:39 +0700

    Update

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
+        require("conform").setup({
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        })
+    end
+}
