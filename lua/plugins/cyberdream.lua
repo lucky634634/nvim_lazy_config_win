2e4d976882dfdff1651a91a66eb03fadedb4ddd0
lucky634634 <khoa21102003@gmail.com>
2025-06-05 22:05:18 +0700

    Update

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -2,4 +2,10 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+        require('cyberdream').setup({
+            transparent = false,
+            italic_comments = true,
+        })
+    end
 }
