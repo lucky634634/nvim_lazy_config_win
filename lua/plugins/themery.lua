004cfec534f44290dab627ae3447830539157544
lucky634634 <khoa21102003@gmail.com>
2025-10-17 20:46:52 +0700

    Update

---
+++ lua/plugins/themery.lua
@@ -0,0 +1,18 @@
+return {
+    "zaldih/themery.nvim",
+    lazy = false,
+    opts = {
+        themes = {
+            "tokyonight",
+            "tokyonight-storm",
+            "tokyonight-night",
+            "tokyonight-moon",
+            "catppuccin",
+            "catppuccin-latte",
+            "catppuccin-frappe",
+            "catppuccin-macchiato",
+            "dracula",
+        },
+        livePreview = true,
+    }
+}
