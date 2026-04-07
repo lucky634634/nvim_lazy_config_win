0b7d7c1125f0707f5ceeec8fe0800e5bac561659
lucky634634 <khoa21102003@gmail.com>
2026-04-07 14:14:35 +0700

    Updat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,22 @@
 vim.pack.add({
-    { src = 'https://github.com/nvim-mini/mini.icons',   version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.files',   version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.tabline', version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.icons',      version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.files',      version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.tabline',    version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.statusline', version = 'stable' },
     "https://github.com/mason-org/mason.nvim",
     "https://github.com/mason-org/mason-lspconfig.nvim",
-    { src = "https://github.com/saghen/blink.cmp", version = 'v1' },
+    { src = "https://github.com/saghen/blink.cmp",       version = vim.version.range("v1.x") },
     "https://github.com/rafamadriz/friendly-snippets",
     "https://github.com/ibhagwan/fzf-lua",
+    { src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x") },
 })
 
 require("plugins.mini-icons")
 require("plugins.mini-files")
 require("plugins.mini-tabline")
+require("plugins.mini-statusline")
 require("plugins.mason")
 require("plugins.mason-lspconfig")
 require("plugins.blink")
 require("plugins.fzf-lua")
+require("plugins.nvim-surround")
